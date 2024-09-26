923ca9c28fdee8d854634d34498f880ae1879d39
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-09-26 12:00:33 -0400

    freed memory in main and palindrome
    Co-authored-by: Dylan Packer <packe089@users.noreply.github.com>

--- palindrome/main.c
+++ palindrome/main.c
@@ -20,8 +20,11 @@ int main(int argc, char *argv[]) {
     // Without it, nothing will ever be a palindrome, unless it happens
     // to somehow start with a '\n'.
     line[strcspn(line, "\n")] = '\0';
-    printf("Is the string <%s> a palindrome? %s\n", line, palindrome(line));
+    char *result = palindrome(line);
+    printf("Is the string <%s> a palindrome? %s\n", line, result);
+    free((void*)result);
   }
 
   free(line);
+  return 0;
 }
